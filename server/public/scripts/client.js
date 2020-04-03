046a22aa7cd54ee201536a4c7fe27db24c5a3596
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-03 16:01:13 -0600

    submit button!

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,14 @@ $(document).ready(init);
 
 function init() {
   console.log("JQ running");
+
+  $("#js-submit-task").on("submit", submitTask);
+}
+
+function submitTask(event) {
+  event.preventDefault();
+
+  const taskInput = $("#js-input-list").val();
+
+  console.log(taskInput);
 }
