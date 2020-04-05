3f83d8be3d2836d2fea950483280cbadb356efc5
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-04 21:06:09 -0600

    posts task to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,17 +1,46 @@
 $(document).ready(init);
 
-let task = [];
+function init() {
+  console.log("LETS GO!");
+}
+
+// let task = [];
 
 $("#js-submit-task").on("submit", submitTask);
-$("#js-task-completion").on("click", ".js-btn-delete-task", deleteTask);
-$(".js-task-completion").on("click", ".js-btn-completed-task", completedTask);
+// $("#js-task-completion").on("click", ".js-btn-delete-task", deleteTask);
+// $(".js-task-completion").on("click", ".js-btn-completed-task", completedTask);
 
 function submitTask(event) {
   event.preventDefault();
 
   const taskInput = $("#js-input-list").val();
 
-  clearTask();
-
   console.log(taskInput);
+  clearTask();
 }
+
+function clearTask() {
+  $("#js-input-list").val("");
+}
+
+// let taskToBeCompleted = {
+//   task: $("#taskIn").val(),
+//   taskCompleted: $("#taskCompletedIn").val(),
+// };
+
+// console.log(taskToBeCompleted);
+
+// $.ajax({
+//   method: "POST",
+//   url: "/todo",
+//   data: taskToDo,
+// })
+//   .then(function (response) {
+//     console.log(response);
+//     getKoalas();
+//   })
+//   .catch(function (error) {
+//     console.log("error in koala post", error);
+//   });
+
+// render to DOM
